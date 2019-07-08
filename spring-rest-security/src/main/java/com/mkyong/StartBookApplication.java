bd8fa99caea81da8f48e39095105f62fc8490344
joycemazumder123 <joycemazumder@hotmail.com>
2019-07-08 21:57:28 +0530

    updated

--- spring-rest-security/src/main/java/com/mkyong/StartBookApplication.java
+++ spring-rest-security/src/main/java/com/mkyong/StartBookApplication.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @SpringBootApplication
 public class StartBookApplication {
 
-    // start everything
+    // start spring boot everything
     public static void main(String[] args) {
         SpringApplication.run(StartBookApplication.class, args);
     }
